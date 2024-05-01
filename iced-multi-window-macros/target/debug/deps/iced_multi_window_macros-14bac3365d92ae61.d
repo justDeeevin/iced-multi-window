543d112ab901afb62c0ba96918715698a3cb56af
Devin Droddy <devin.droddy@gmail.com>
2024-05-01 16:25:47 -0400

    ignore macros/target

--- iced-multi-window-macros/target/debug/deps/iced_multi_window_macros-14bac3365d92ae61.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Devin Droddy\Software Dev\iced-multi-window\iced-multi-window-macros\target\debug\deps\libiced_multi_window_macros-14bac3365d92ae61.rmeta: src\lib.rs Cargo.toml
-
-C:\Users\Devin Droddy\Software Dev\iced-multi-window\iced-multi-window-macros\target\debug\deps\iced_multi_window_macros-14bac3365d92ae61.d: src\lib.rs Cargo.toml
-
-src\lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
