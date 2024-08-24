88a93171b113bb0ffa64250519494b0b81846554
Devin Droddy <devin.droddy@gmail.com>
2024-08-24 14:55:43 -0400

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs:
+    with inputs; let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      toolchain = fenix.packages.${system}.complete.toolchain;
+    in
+    {
+      devShells.x86_64-linux = {
+        default = pkgs.mkShell {
+          # Libraries
+          buildInputs = [ ];
+          # Additional tooling
+          packages = [ toolchain ];
+        };
+      };
+    };
+}
